2d78347057e7f31d6c8c4eea065df8c57da56a6b
Kelsie Besinger <kelsiebesinger@gmail.com>
2021-05-19 11:52:27 -0600

    Add dummy conact data

---
+++ contactData.js
@@ -0,0 +1,32 @@
+const contactData = [
+  {
+    id: 1,
+    name: "Snow White",
+    phone: "212-555-5555",
+    email: "snow123@example.com",
+  },
+  {
+    id: 2,
+    name: "Barry Manilow",
+    phone: "212-111-1111",
+    email: "barry123@example.com",
+  },
+  {
+    id: 3,
+    name: "Spongebob Squarepants",
+    phone: "212-333-3333",
+    email: "pineapple123@example.com",
+  },
+  {
+    id: 4,
+    name: "Casper the Ghost",
+    phone: "212-222-2222",
+    email: "boo123@example.com",
+  },
+  {
+    id: 5,
+    name: "Donald Duck",
+    phone: "212-444-4444",
+    email: "duck123@example.com",
+  },
+];
