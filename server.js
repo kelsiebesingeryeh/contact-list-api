1fcfc043d51615ae77c884dc2a39241a16b15c38
Kelsie Besinger <kelsiebesinger@gmail.com>
2021-05-19 12:22:38 -0600

    Add babel config file

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const app = express();
 const contactData = require('./contactData.js');
+const cors = require("cors");
 
 
 app.use(express.json());
+app.use(cors());
 app.set('port', process.env.PORT || 3000);
 app.locals.contactInformation = contactData;
 
